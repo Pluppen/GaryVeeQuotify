72d1b89b1bf450108d4e3b0951a556d40760213d
Elias Juremalm <eliass.juremalm@gmail.com>
2020-10-31 14:39:22 +0100

    Inital commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const video = document.getElementById("video")
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo)
+
+function startVideo(){
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                video.srcObject = stream;
+            })
+            .catch(function (err0r) {
+                console.log("Something went wrong!");
+                console.error(err0r);
+            });
+    }
+}
+
+video.addEventListener('play', () => {
+    document.getElementById("loader").style.display = "none";
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        let mood = {name: "", value: 100}
+        for(const [key, value] of Object.entries(detections[0].expressions)){
+            let diff = 1 - value
+            if(diff < mood.value && diff >= 0){
+                mood.name = key
+                mood.value = diff
+            }
+        }
+        document.getElementById("mood").innerHTML = mood.name;
+        document.body.className = mood.name;
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    }, 100)
+})
